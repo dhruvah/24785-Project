ee99e13ea60ddba3fb28283d5c16290b863482f5
Siqiao Fu <siqiaof@andrew.cmu.edu>
2021-11-25 01:44:00 +0800

    The LM algorithm was added and tested on carSequence. The LM algorithm and the original GN are written in the same file.

--- code/LucasKanade.py
+++ code/LucasKanade.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 
-def LucasKanade(It, It1, rect, threshold, num_iters, p0=np.zeros(2)):
+
+# Gauss-Newton
+def LucasKanade_GN(It, It1, rect, threshold, num_iters, p0=np.zeros(2)):
     """
     :param It: template image
     :param It1: Current image
@@ -12,7 +14,6 @@ def LucasKanade(It, It1, rect, threshold, num_iters, p0=np.zeros(2)):
     :return: p: movement vector [dp_x, dp_y]
     """
 
-    # Put your implementation here
     p = p0
     
     x1, y1, x2, y2 = rect[0], rect[1], rect[2], rect[3]
@@ -30,30 +31,84 @@ def LucasKanade(It, It1, rect, threshold, num_iters, p0=np.zeros(2)):
     count = 0
     x = np.arange(x1, x2 + 1)
     y = np.arange(y1, y2 + 1)
-    x, y = np.meshgrid(x,y)
+    X, Y = np.meshgrid(x,y)
+    Itx = It_spline.ev(Y, X).flatten()
 
-    alpha = 10
-    
     while np.linalg.norm(dp)>= threshold and count <= num_iters:
         count += 1
         
-        Itx = It_spline.ev(y, x).flatten()
+        
         It1x_warp = It1_spline.ev(y+p[1], x+p[0]).flatten()
         G = np.array([
             It1_spline.ev(y+p[1], x+p[0], dx = 0, dy = 1).flatten(),
             It1_spline.ev(y+p[1], x+p[0], dx = 1, dy = 0).flatten()]).T
-        
-        # # print(np.shape(G))
-        # b = (Itx - It1x_warp).flatten()
-        # # print(np.shape(b))
-        # dp = (np.linalg.inv(G.T @ G)) @ (G.T @ b)
-        
-        H = G.T @ G
-        dp = np.linalg.inv(H) @ G.T @ (Itx - It1x_warp) # Gauss-Newton
-        # dp = alpha * (G.T @ (Itx - It1x_warp))          # Gradient Descent
 
-        p+=dp
+        b = Itx - It1x_warp
+        H = G.T @ G
+        dp = np.linalg.inv(H) @ G.T @ b
+
+        p += dp
+
+    return p
+
+
+# Levenberg-Marquardt
+def LucasKanade_LM(It, It1, rect, threshold, num_iters, p0=np.zeros(2), delta=0.01):
+    """
+    :param It: template image
+    :param It1: Current image
+    :param rect: Current position of the car (top left, bot right coordinates)
+    :param threshold: if the length of dp is smaller than the threshold, terminate the optimization
+    :param num_iters: number of iterations of the optimization
+    :param p0: Initial movement vector [dp_x0, dp_y0]
+    :return: p: movement vector [dp_x, dp_y]
+    """
+
+    p = p0
+    dp = np.array([float("inf"), float("inf")])
+    
+    x1, y1, x2, y2 = rect[0], rect[1], rect[2], rect[3]
+    
+    It_spline=RectBivariateSpline(np.arange(It.shape[0]),
+                                  np.arange(It.shape[1]), 
+                                  It)
+    
+    It1_spline=RectBivariateSpline(np.arange(It1.shape[0]),
+                                   np.arange(It1.shape[1]),
+                                   It1)
+    
+    x = np.arange(x1, x2 + 1)
+    y = np.arange(y1, y2 + 1)
+    x, y = np.meshgrid(x,y)
+
+    count = 0
+    Itx = It_spline.ev(y, x).flatten()
+    prev_error = -1
+
+    while np.linalg.norm(dp)>= threshold and count <= num_iters:
+        count += 1
+
+        It1x_warp = It1_spline.ev(y+p[1], x+p[0]).flatten()
+        b = Itx - It1x_warp
+        error = np.linalg.norm(b)
+
+        if prev_error != -1:
+            if error < prev_error:
+                delta /= 10
+            else:
+                p -= dp
+                It1x_warp = It1_spline.ev(y+p[1], x+p[0]).flatten()
+                delta *= 10
+        prev_error = error
+
+        G = np.array([
+            It1_spline.ev(y+p[1], x+p[0], dx = 0, dy = 1).flatten(),
+            It1_spline.ev(y+p[1], x+p[0], dx = 1, dy = 0).flatten()]).T
         
+        H_LM = G.T @ G + delta * np.array([[np.sum(G[:,0]**2), 0], [0, np.sum(G[:,1])**2]])
+        dp = np.linalg.inv(H_LM) @ G.T @ b
+
+        p += dp
     
     return p
     
--- code/LucasKanadeAffine.py
+++ code/LucasKanadeAffine.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 
-def LucasKanadeAffine(It, It1, threshold, num_iters):
+
+# Gauss-Newton
+def LucasKanadeAffine_GN(It, It1, rect, threshold, num_iters):
     """
     :param It: template image
     :param It1: Current image
@@ -12,10 +14,9 @@ def LucasKanadeAffine(It, It1, threshold, num_iters):
 
     # put your implementation here
     M = np.eye(3)
-    
     p = M[0:2, 0:3].flatten()
     
-    x1, y1, x2, y2 = 0, 0, It.shape[1] -1, It.shape[0] -1
+    x1, y1, x2, y2 = rect[0], rect[1], rect[2], rect[3]
     
     It_spline=RectBivariateSpline(np.arange(It.shape[0]),
                                   np.arange(It.shape[1]), 
@@ -25,51 +26,109 @@ def LucasKanadeAffine(It, It1, threshold, num_iters):
                                    np.arange(It1.shape[1]),
                                    It1)
     
-    dp = np.array([float("inf")]* 6)
+    dp = np.array([float("inf")] * 6)
     
     count = 0
-   
-    
+    x = np.arange(x1, x2 + 1)
+    y = np.arange(y1, y2 + 1)
+    X, Y = np.meshgrid(x,y)
+    Itx = It_spline.ev(Y, X).flatten()
+
     while np.linalg.norm(dp)>= threshold and count <= num_iters:
-        x = np.arange(x1, x2 + 1)
-        y = np.arange(y1, y2 + 1)
-        X, Y = np.meshgrid(x,y)
+
         count += 1
         
         warpX = p[0] * X + p[1] * Y + p[2]
         warpY = p[3] * X + p[4] * Y + p[5]
         
-        commonpts = np.where((warpX >= x1) & (warpX <= x2) & 
-                             (warpY >= y1) & (warpY <= y2), True, False) 
+        dwx = It1_spline.ev(warpY, warpX, dx = 0, dy = 1).flatten()
+        dwy = It1_spline.ev(warpY, warpX, dx = 1, dy = 0).flatten()
         
-        X, Y = X[commonpts], Y[commonpts]
-        warpX, warpY = warpX[commonpts], warpY[commonpts]
+        It1x_warp = It1_spline.ev(warpY, warpX).flatten()
+        
+        G = np.array([dwx * X.flatten(),
+                      dwx * Y.flatten(),
+                      dwx,
+                      dwy * X.flatten(),
+                      dwy * Y.flatten(),
+                      dwy]).T
+        
+        b = Itx - It1x_warp
+        
+        H = G.T @ G
+        dp = (np.linalg.inv(H)) @ (G.T @ b)
+        p += dp
+
+    return p
+
+
+# Levenberg-Marquardt
+def LucasKanadeAffine_LM(It, It1, rect, threshold, num_iters, delta=0.01):
+    """
+    :param It: template image
+    :param It1: Current image
+    :param threshold: if the length of dp is smaller than the threshold, terminate the optimization
+    :param num_iters: number of iterations of the optimization
+    :return: M: the Affine warp matrix [3x3 numpy array] put your implementation here
+    """
+
+    # put your implementation here
+    M = np.eye(3)
+    p = M[0:2, 0:3].flatten()
+    
+    x1, y1, x2, y2 = rect[0], rect[1], rect[2], rect[3]
+    
+    It_spline=RectBivariateSpline(np.arange(It.shape[0]),
+                                  np.arange(It.shape[1]), 
+                                  It)
+    
+    It1_spline=RectBivariateSpline(np.arange(It1.shape[0]),
+                                   np.arange(It1.shape[1]),
+                                   It1)
+    
+    dp = np.array([float("inf")] * 6)
+    
+    count = 0
+    x = np.arange(x1, x2 + 1)
+    y = np.arange(y1, y2 + 1)
+    X, Y = np.meshgrid(x,y)
+    Itx = It_spline.ev(Y, X).flatten()
+    prev_error = -1
+
+    while np.linalg.norm(dp)>= threshold and count <= num_iters:
+
+        count += 1
+        
+        warpX = p[0] * X + p[1] * Y + p[2]
+        warpY = p[3] * X + p[4] * Y + p[5]
         
         dwx = It1_spline.ev(warpY, warpX, dx = 0, dy = 1).flatten()
         dwy = It1_spline.ev(warpY, warpX, dx = 1, dy = 0).flatten()
         
-        It1x_warp = It1_spline.ev(warpY, warpX)
+        It1x_warp = It1_spline.ev(warpY, warpX).flatten()
         
-        A = np.array([dwx*X,
-                      dwx*Y,
+        b = Itx - It1x_warp
+        error = np.linalg.norm(b)
+
+        if prev_error != -1:
+            if error < prev_error:
+                delta /= 10
+            else:
+                p -= dp
+                It1x_warp = It1_spline.ev(y+p[1], x+p[0]).flatten()
+                delta *= 10
+        prev_error = error
+
+        G = np.array([dwx * X.flatten(),
+                      dwx * Y.flatten(),
                       dwx,
-                      dwy*X,
-                      dwy*Y,
+                      dwy * X.flatten(),
+                      dwy * Y.flatten(),
                       dwy]).T
         
-        b = (It[commonpts] - It1x_warp).flatten()
-        # print(np.shape(A))
-    
-        
-        # print(np.shape(b))
-        
-        dp = (np.linalg.inv(A.T @ A)) @ (A.T @ b)
+        LM_term = np.diag(np.sum(G**2, axis=0))
+        H_LM = G.T @ G + delta * LM_term
+        dp = (np.linalg.inv(H_LM)) @ (G.T @ b)
+        p += dp
 
-        # H = A.T @ A
-        # dp = np.linalg.inv(H) @ A.T @ (It[commonpts] - It1x_warp)
-        # print(np.shape(dp))
-        p+=dp
-        
-    M = np.vstack((p.reshape(2,3), M[2,0:3]))
-
-    return M
+    return p
